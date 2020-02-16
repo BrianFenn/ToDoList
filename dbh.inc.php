52c9c7a807cce3164f688bd8446cd4cb9cd02245
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-02-15 16:46:44 -0800

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "Kholmes1#";
+$dbPassword = "";
 $dbName = "ToDoList";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
